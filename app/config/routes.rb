098da29374b71a3edbf18ac14a4dbe547a676261
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2023-05-12 12:32:38 +0300

    Summary

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :regions
   resources :countries
+  resources :cities
+  resources :campings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
